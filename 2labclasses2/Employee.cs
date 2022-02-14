83f82d2418cad6e2b10e8c13bfd5f0ef58438d91
massiveo1k <N.Synelnyk.FIT.125.20@knute.edu.ua>
2022-02-14 14:32:35 +0200

    Добавьте файлы проекта.

---
+++ 2labclasses2/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2labclasses2
+{
+    internal class Employee
+    {
+        string name;
+        string surname;
+        DateTime dateOfHire;
+        double salary;
+
+        public enum Position
+        {
+            Assistant = 1000000,
+            President = 5000000
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfHire
+        {
+            get
+            {
+                return dateOfHire;
+            }
+            set
+            {
+                if (dateOfHire.Date < DateTime.Parse("14.02.2021"))
+                {
+                    dateOfHire = value;
+                }
+                else
+                {
+                    Console.WriteLine("Date before times!");
+                }
+
+            }
+        }
+
+        public Employee(string name, string surname, DateTime dateOfHire)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfHire = dateOfHire;
+        }
+
+        public double DiscoverGrade(DateTime dateOfHire)
+        {
+            double dateValueForGrade = (DateTime.Now - dateOfHire).TotalDays;
+
+            if (dateValueForGrade >= 2000 && dateValueForGrade < 4000)
+                return 3.5;
+            else if (dateValueForGrade >= 4000)
+                return 3.0;
+            else
+                return 2.0;
+        }
+
+        public double Calculation(Position position)
+        {
+            double grade = DiscoverGrade(dateOfHire);
+            switch (position)
+            {
+                case Position.Assistant:
+                    salary = 1000000 * grade;
+                    return salary;
+                case Position.President:
+                    salary = 5000000 * grade;
+                    return salary;
+                default:
+                    Console.WriteLine("No such position!");
+                    break;
+
+            }
+            return 0.0;
+        }
+    }
+}
---
+++ 2labclasses2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2labclasses2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee one = new Employee("John", "Kennedy", DateTime.Parse("14.02.2027"));
+            Console.WriteLine("Name is {0}, surname is {1}. Date of hire: {2}", one.Name, one.Surname, one.DateOfHire);
+            double salary = one.Calculation(Employee.Position.President);
+            Console.WriteLine("Salary is {0}, tax is {1}, position is {2}.", salary, salary * 0.45, Employee.Position.President);
+            Console.ReadKey();
+        }
+    }
+
+}
